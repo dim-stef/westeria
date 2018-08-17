2bbbf9c9b0da1f0c701b31dbfe10e18880c9111a
Dimitris Stefanakis <jimstef@outlook.com>
2018-08-18 00:12:17 +0300

    Importing signals in ready function

--- groups/apps.py
+++ groups/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class GroupsConfig(AppConfig):
     name = 'groups'
+
+    def ready(self):
+        # noinspection PyUnresolvedReferences
+        import groups.signals
