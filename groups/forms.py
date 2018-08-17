afa61242e5471cf8f4acd006b0c842033a6ff0d0
Dimitris Stefanakis <jimstef@outlook.com>
2018-08-18 00:11:56 +0300

    Added form validation, GroupBranchForm

--- groups/forms.py
+++ groups/forms.py
@@ -9,8 +9,22 @@ class GroupForm(forms.ModelForm):
         model = Group
         exclude = ['owner', 'id', 'tag']
 
-        error_messages = {
-            NON_FIELD_ERRORS: {
-                'unique_together': "You already have a group named %(name).",
-            }
-        }
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        super().clean()
+        if Group.objects.filter(owner=self.user, name=self.cleaned_data['name']).exists():
+            self.add_error("name", "You already have a group named %s" % self.cleaned_data['name'])
+
+
+class GroupBranchForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['id']
+
+    def clean(self):
+        super().clean()
+        if self.cleaned_data['parents'] in self.parents.all():
+            raise ValidationError({'sameGroup': ["same group"]})
