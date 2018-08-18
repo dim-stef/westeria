1f739c042d20eba5a0e4bb99cb00aa0b8167e7a4
Dimitris Stefanakis <jimstef@outlook.com>
2018-08-18 22:25:27 +0300

    Google url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CoreView
+from .views import CoreView, GoogleView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', CoreView.as_view(), name='home'),
+    path('google0677b618caac6923.html', GoogleView.as_view()),
 ]
