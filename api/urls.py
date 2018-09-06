738095bc8c288221906dabc87a8372764230cadf
Dimitris Stefanakis <jimstef@outlook.com>
2018-09-06 18:21:24 +0300

    Added group API and individual ROOT url

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
-from .views import UserViewSet, UserProfileViewSet
-from rest_framework import routers
+from django.urls import path
 from django.conf.urls import url
+from rest_framework import routers
+from .views import UserViewSet, UserProfileViewSet, GroupViewSet, GroupRootViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, base_name='users')
 router.register(r'profile', UserProfileViewSet, base_name='profile')
+router.register(r'groups', GroupViewSet, base_name='group')
+router.register(r'ROOT', GroupRootViewSet, base_name='grouproot')
 
 
 urlpatterns = [
