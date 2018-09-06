24eb03da90ca7a78963550810b162797453bdbfb
Dimitris Stefanakis <jimstef@outlook.com>
2018-09-06 18:26:04 +0300

    Added entries for profileApp, groupApp

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,7 +1,11 @@
     const path = require("path");
     const HtmlWebPackPlugin = require("html-webpack-plugin");
     module.exports = {
-      entry: ["./src/js/app1.js"],
+      //entry: ["./src/js/profileApp.js"],
+      entry: {
+        'profileApp': './src/js/profileApp.js',
+        'groupApp': './src/js/groupApp.js',
+      },
       output: {
         path: path.resolve(__dirname, "dist"),
         filename: "js/[name].js"
