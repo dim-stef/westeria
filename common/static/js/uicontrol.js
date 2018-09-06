2ae43962750b2c8c4ba91fd74d7b107a31704d00
Dimitris Stefanakis <jimstef@outlook.com>
2018-09-06 18:36:35 +0300

    Added jsplumb features

--- common/static/js/uicontrol.js
+++ common/static/js/uicontrol.js
@@ -3,23 +3,15 @@
 $(document).ready(function () {
     var pathname = window.location.pathname;
     window.history.pushState({ page: pathname}, 'Subranch', pathname);
-    /*if(window.location.pathname == '/creategroup/'){
-        document.getElementById("modal-window").classList.toggle("modal-window");
-        document.getElementById("group-container").classList.toggle("show");
-    }*/
-})
+});
 
 
-
+$( window ).resize(function() {
+   jsPlumb.repaintEverything(); //firstInstance.repaintEverything();
+});
 /*
 Profile picture control*/
 $(function () {
-
-    /*$('<a class="prof" id = "prof"></a>').appendTo('#container-side');
-    $('<div id="inner-container" class = "inner-container"></div>').appendTo('#container-side');
-    $('<button class="feed" id = "feed">feed</button>').appendTo('#inner-container');
-    $('<button class="my-groups" id = "my-groups">my-groups</button>').appendTo('#inner-container');
-    $('<button class="map" id = "map">map</button>').appendTo('#inner-container');*/
 
     if ($(window).width() < 1200) {
         $('#prof').width($('#prof').height());
@@ -42,10 +34,11 @@ $(function () {
 
 
 
-
+/*If page loaded from < 1200px screen*/
 $(function () {
     if ($(window).width() < 1200) {
         $("#content").appendTo("#mobile-content-container");
+        jsPlumb.repaintEverything();
     }
 });
 
@@ -72,5 +65,16 @@ $(function () {
         document.getElementById("main-wrapper").classList.toggle("content-drawer");
         document.getElementById("container-side").classList.toggle("slider");
         document.getElementById("prof").classList.toggle("prof-remove-shadow");
+        jsPlumb.repaintEverything();
     });
+});
+
+
+$(function () {
+    var checkContents = setInterval(function(){
+        //console.log("in");
+        if ($("#global").length === 0){
+            jsPlumb.repaintEverything();
+        }
+    },1000);
 });
