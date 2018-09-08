2b349ceda7c0b0ff18d04b417d43f9743e783177
Dimitris Stefanakis <jimstef@outlook.com>
2018-09-09 02:29:53 +0300

    Code format

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,7 @@ from groups.models import Group
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'last_login', 'email', 'is_active', 'is_staff']  #'__all__'
+        fields = ['id', 'last_login', 'email', 'is_active', 'is_staff']  # '__all__'
         read_only_fields = ['id', 'last_login', 'email', 'is_active', 'is_staff']
 
 
@@ -20,7 +20,7 @@ class UserAdminSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['user', 'url', 'profile_image', 'fake_count']  #'__all__'
+        fields = ['user', 'url', 'profile_image', 'fake_count']  # '__all__'
         read_only_fields = ['user', 'fake_count']
 
     def update(self, instance, validated_data):
