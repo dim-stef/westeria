791dce815cb909ca4568d68bed340f66cffcab05
Dimitris Stefanakis <jimstef@outlook.com>
2018-10-29 01:51:24 +0200

    Nested api urls

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,24 @@
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework import routers
-from .views import UserViewSet, UserProfileViewSet, GroupViewSet, GroupRootViewSet
+from rest_framework_nested import routers
+from .views import UserViewSet, UserProfileViewSet, GroupViewSet, GroupRootViewSet, ChildrenViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, base_name='users')
 router.register(r'profile', UserProfileViewSet, base_name='profile')
-router.register(r'groups', GroupViewSet, base_name='group')
 router.register(r'ROOT', GroupRootViewSet, base_name='grouproot')
+router.register(r'groups', GroupViewSet, base_name='group')
 
+group_router = routers.NestedSimpleRouter(router, r'groups')
+group_router.register(r'children', ChildrenViewSet, base_name='children')
+
+#children_router = routers.NestedSimpleRouter(group_router, r'children', lookup='uri')
 
 urlpatterns = [
-
+    url(r'^', include(router.urls)),
+    url(r'^', include(group_router.urls)),
+    #url(r'^', include(children_router.urls)),
 ]
 
-urlpatterns += router.urls
+#urlpatterns += router.urls
