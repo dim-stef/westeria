e72df8139533794ad4c67fba6e44e1f72f4763f6
Dimitris Stefanakis <jimstef@outlook.com>
2018-10-29 01:59:57 +0200

    Bug fixes and mobile group component

--- react/src/js/components/presentational/Group.js
+++ react/src/js/components/presentational/Group.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom'
-import PropTypes from "prop-types";
+import sizeMe from "react-sizeme"
 import { NodeContainer, Tree } from "../container/GroupContainer"
+import { timingSafeEqual } from "crypto";
 const uuidv1 = require('uuid/v1');
 
 const REACT_VERSION = React.version;
@@ -17,7 +18,7 @@ function connector(parent, child) {
                 connector: ["Flowchart"],
                 source: parent,
                 target: child,
-                anchor: ["Bottom", "Top"],
+                anchor: ["Right", "Left"],
                 endpoint: "Blank",
             });
         });
@@ -25,9 +26,10 @@ function connector(parent, child) {
 }
 
 function treeOverflow() {
-    var treeContainerWidth = document.getElementById("tree-container").scrollWidth;
+    var treeWidth = document.getElementById("tree").scrollWidth;
     var mapContainerWidth = document.getElementById("map-container").clientWidth;
-    if (treeContainerWidth > mapContainerWidth) {
+    console.log(treeWidth, mapContainerWidth)
+    if (treeWidth > mapContainerWidth) {
         return true;
     }
 }
@@ -45,7 +47,8 @@ class ExtendButton extends Component {
 
     handleClick() {
         console.log(this.state.newRoot);
-        this.props.updateNodeContainer(this.state.newRoot);
+        jsPlumb.deleteEveryEndpoint();
+        //this.props.updateNodeContainer(this.state.newRoot);
     }
 
     componentDidUpdate() {
@@ -58,10 +61,12 @@ class ExtendButton extends Component {
         return (
             //<NodeContainer root={null} msg={")))))"} key="1"/>
             //<Node groups={null} key="1"/>
-            <button
-                style={{ visibility: this.props.visibility }}
-                onClick={this.handleClick}>
-            </button>
+            <Link to={"/map/" + this.state.newRoot} onClick={this.handleClick}>
+                <button
+                    style={{ visibility: this.props.visibility }} onClick={this.handleClick}>
+
+                </button>
+            </Link>
         )
     }
 }
@@ -74,10 +79,11 @@ class Group extends Component {
             group: this.props.group,
             name: this.props.name,
             childKey: this.props.childKey,
-            display: "inline-block",
+            display: "flex", //change to inline-block for horizontal tree view
             visibility: "visible",
             hiddenChildren: 0,
-            buttonVisible: "visible"
+            buttonVisible: "visible",
+            hidden: false
         }
         this.updateParent = this.updateParent.bind(this);
     }
@@ -89,29 +95,14 @@ class Group extends Component {
         }))
     }
 
-    componentDidMount() {
+    componentDidMount(){
         if (this.state.name !== "ROOT" && this.state.name !== "global") {
             connector(this.props.parentKey, this.state.childKey);
         }
-
-        if (this.domElement) {
-            //console.log(this.domElement.getBoundingClientRect())
-        }
+        console.log("group mount")
+        console.log(this.domElement)
+        
     }
-    /*componentDidMount(){
-        if (treeOverflow() === true) {
-            this.setState({ visibility: "hidden" }, () => {
-                //console.log(this.props)
-                if(this.props.updateParent){
-                    this.props.updateParent();
-                }
-           });
-        }
-
-        if (this.state.name !== "ROOT" && this.state.name !== "global") {
-            connector(this.props.parentKey, this.state.childKey);
-        }
-    }*/
 
     componentDidUpdate() {
         if (this.state.name !== "ROOT" && this.state.name !== "global") {
@@ -139,28 +130,35 @@ class Group extends Component {
             return (null);
         }
 
+        if(this.state.hidden){
+            return (null);
+        }
         return (
-            <li style={{ display: this.state.display }} ref={(el) => { this.domElement = el }}>
+            <li ref={(el) => { this.domElement = el }} style={{ display: this.state.display }}>
                 <div class="item-container">
                     <li id="id" class="group-link">
-                        <Link to={"/" + this.props.id}>Home</Link>
+
                     </li>
-                    <a href="#" class="group-link">
+                    <Link to={"/" + this.props.id} className="group-link">
                         <div class="group-container" id={this.state.childKey}>
-                            <div class="group"></div>
-                            <span>{this.state.name}</span>
+                            <div style={{width:200, backgroundColor:"#efefef", display:"inline-block",paddingTop: '40.25%',position: 'relative'}}>
+                                <div class="group" style={{float:"left"}}></div>
+                            </div>
+                            <span style={{display: 'block'}}>{this.state.name}</span>
                         </div>
-                    </a>
+                    </Link>
                     <ExtendButton
                         key={this.state.childKey}
                         visibility={this.state.buttonVisible}
-                        newRoot={this.state.group.id}
+                        newRoot={this.state.group.uri}
                         updateNodeContainer={this.props.updateNodeContainer} />
                 </div>
                 {React.cloneElement(this.props.children, { updateParent: this.updateParent })}
             </li>
         );
     }
+        
+    
 }
 
 export class Node extends Component {
@@ -170,6 +168,7 @@ export class Node extends Component {
         this.state = {
             groups: null,
             root: this.props.groups,
+            more:false
         }
     }
 
@@ -189,39 +188,32 @@ export class Node extends Component {
                     key={childKey}
                     id={c.uri}
                     name={c.name}
-                    childKey={childKey}
-                    parentKey={key}
+                    childKey={c.key}
+                    parentKey={this.state.root.key}
                     updateParent={this.props.updateParent}
                     updateNodeContainer={this.props.updateNodeContainer}>
                     <Node groups={c} updateNodeContainer={this.props.updateNodeContainer} />
                 </Group>
             );
         });
-
-        //this.setState({groups:groups})
-
-        
         return groups;
     }
 
-    componentWillReceiveProps(nextProps) {
-        jsPlumb.deleteEveryEndpoint();
+    /*componentWillReceiveProps(nextProps) {
+        console.log("received props")
+        //jsPlumb.deleteEveryEndpoint();
         this.setState({ root: nextProps.groups });
-    }
+    } 
+    *///UNCOMMENT IF SUNCHRONOYS FETCHING
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         console.log("unmount")
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log(prevProps,this.props,prevState)
-        console.log("updated")
         jsPlumb.repaintEverything();
     }
 
     render() {
         console.log("node rendered")
-        jsPlumb.repaintEverything();
+        //jsPlumb.repaintEverything();
         var groups = this.collectGroups();
         return (
             <ul>
@@ -230,4 +222,54 @@ export class Node extends Component {
         );
 
     }
+}
+
+export class MobileGroups extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            groups:this.props.groups
+        }
+    }
+
+    collectGroups(){
+        let groups = this.state.groups.children.map((c)=>{
+            var styleName = '';
+            return (
+                <PureGroup styleName={styleName} style={{marginTop:10,marginBottom:0}} group={c}/>
+            )
+        })
+        return groups;
+    }
+
+    render(){
+        var groups = this.collectGroups();
+        console.log(groups)
+        var parent = this.state.groups.parent;
+        var styleName = 'parent';
+        return (
+            <div style={{display: 'flex', flexFlow:'column', width:'100%', alignItems:'center'}}>
+                <PureGroup styleName={styleName} style={{marginTop:0,marginBottom:30}} group={parent}/>
+                {groups}
+            </div>
+        )
+    }
+}
+
+
+const PureGroup = ({styleName, style, group}) => {
+    return(
+        <div className={`item-container ${ styleName }`} style={{marginTop:style.marginTop,marginBottom:style.marginBottom}}>
+            <Link to={"/" + group.uri} className="group-link">
+                <div class="group-container" id={group.id}>
+                    <div style={{width:'100%', backgroundColor:"#efefef", display:"inline-block", paddingTop: '27.25%', position: 'relative'}}>
+                        <div class="group"></div>
+                    </div>
+                    <span style={{display: 'block'}}>{group.name}</span>
+                </div>
+            </Link>
+            <ExtendButton
+                newRoot={group.uri}/>
+        </div>
+    )
 }
