8cde9f145f3bad253f86e985b39e3acd43cc3cdb
Dimitris Stefanakis <jimstef@outlook.com>
2018-10-29 02:00:14 +0200

    Modified html layout

--- react/src/js/components/home.js
+++ react/src/js/components/home.js
@@ -40,49 +40,64 @@ export class SideBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className="root-wrapper">
                 <div onClick={this.modalClick} id="modal-window" />
                 <div id="creategroup-container">
                 </div>
+                <div class="mobile-menu">
+                    <div style={{width: '100%', position: 'relative', height: 130, backgroundColor: '#266277', zIndex: 5}}>
+                        <div className="mobile-top-bar-container">
+                            <div className="mobile-top-bar">
+                                <div className="search-container">
+                                    <button className="search-button">
+                                        <i class="material-icons noselect" style={{ fontSize:"3em", color:"whitesmoke"}}>search</i>
+                                    </button>
+                                    <input className="search" type="text" placeholder="Search"></input>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div id="mobile-content-container" className="main content">
                 </div>
-                <div className="closed-drawer-container">
-                    <button name="drawer" className="material-icons drawer-btn closed-drawer-btn">menu</button>
-                </div>
-                <div className="mobile-buttons-container">
-                    <button id="feed-mobile" className="mobile-buttons material-icons" style={{ borderRight: '1px solid rgba(0, 0, 0, 0.15)' }}>home
-            </button>
-                    <button id="create-mobile" className="mobile-buttons material-icons" style={{ borderRight: '1px solid rgba(0, 0, 0, 0.15)', borderLeft: '1px solid rgba(0, 0, 0, 0.15)' }}>create
-            </button>
-                    <button id="settings-mobile" className="mobile-buttons material-icons" style={{ borderRight: '1px solid rgba(0, 0, 0, 0.15)', borderLeft: '1px solid rgba(0, 0, 0, 0.15)' }}>
-                        settings
-            </button>
-                    <button name="drawer" id="mobile-drawer" className="mobile-buttons mobile-drawer material-icons">menu</button>
-                </div>
-                <div className="container-side" id="container-side">
-                    <button name="drawer" className="material-icons drawer-btn">menu</button>
-                    <a className="prof" id="prof">
-                    </a>
-                    <div id="inner-container" className="inner-container">
-                        <Link to="/" className="feed" id="feed">
-                            <span className="main-buttons">Feed</span>
-                        </Link>
-                        <Link to="/" className="my-groups" id="my-groups">
-                            <span className="main-buttons">My groups</span>
-                        </Link>
-                        <Link to="/map" className="map" id="map">
-                            <span className="main-buttons">Map</span>
-                        </Link>
+                {/* 
+                    <div className="closed-drawer-container">
+                        <button name="drawer" className="material-icons drawer-btn closed-drawer-btn">menu</button>
                     </div>
-                    <div className="utility-buttons-container">
-                        <Link to="/settings">
-                            <button id="settings" className="material-icons settings-button utility-buttons">settings</button>
-                        </Link>
-                        <button onClick={this.createClick} id="create" className="material-icons utility-buttons">create</button>
+                */}
+                
+                <div className="side-bar-container">
+                    <div className="utility-side-bar">
+                        <div className="utility-top">
+                            <i class="material-icons noselect" style={{fontSize: 44, color: 'white', marginTop: 10}}>search</i>
+                            <button onClick={this.createClick} id="create" className="material-icons utility-buttons" style={{marginTop:40}}>create</button>
+                        </div>
                         <form method="post" action="/logout/">
-                            <CSRFToken />
-                            <button type="submit" id="signout" className="fa fa-sign-out settings-button utility-buttons" style={{ fontSize: 20 }} />
-                        </form>
+                                <CSRFToken />
+                                <button type="submit" id="signout" className="fa fa-sign-out settings-button utility-buttons" style={{ fontSize: 20 }} />
+                            </form>
+                    </div>
+                    <div className="container-side" id="container-side">
+                        {/*<button name="drawer" className="material-icons drawer-btn">menu</button>*/}
+                        <a className="prof" id="prof">
+                        </a>
+                        <div id="inner-container" className="inner-container">
+                            <Link to="/" className="feed" id="feed">
+                                <span className="main-buttons">Feed</span>
+                            </Link>
+                            <Link to="/" className="my-groups" id="my-groups">
+                                <span className="main-buttons">My groups</span>
+                            </Link>
+                            <Link to="/map" className="map" id="map">
+                                <span className="main-buttons">Map</span>
+                            </Link>
+                        </div>
+                        <div className="utility-buttons-container">
+                            <Link to="/settings">
+                                <button id="settings" className="material-icons settings-button utility-buttons">settings</button>
+                            </Link>
+                            
+                        </div>
                     </div>
                 </div>
                 <div id="main-wrapper" className="main-wrapper">
