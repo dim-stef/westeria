ea6f1a48f4c0810d7dcf5f14f7c2d8361439b638
Dimitris Stefanakis <jimstef@outlook.com>
2018-11-08 00:40:44 +0200

    Added new messages and chat subroutes

--- api/urls.py
+++ api/urls.py
@@ -2,23 +2,26 @@ from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework_nested import routers
-from .views import UserViewSet, UserProfileViewSet, GroupViewSet, GroupRootViewSet, ChildrenViewSet
+from . import views
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, base_name='users')
-router.register(r'profile', UserProfileViewSet, base_name='profile')
-router.register(r'ROOT', GroupRootViewSet, base_name='grouproot')
-router.register(r'groups', GroupViewSet, base_name='group')
+router.register(r'users', views.UserViewSet, base_name='users')
+router.register(r'profile', views.UserProfileViewSet, base_name='profile')
+router.register(r'ROOT', views.GroupRootViewSet, base_name='grouproot')
+router.register(r'groups', views.GroupViewSet, base_name='group')  # lookup= 'groups'
 
-group_router = routers.NestedSimpleRouter(router, r'groups')
-group_router.register(r'children', ChildrenViewSet, base_name='children')
+group_router = routers.NestedSimpleRouter(router, r'groups')  # lookup= 'children'
+group_router.register(r'children', views.ChildrenViewSet, base_name='children')
 
-#children_router = routers.NestedSimpleRouter(group_router, r'children', lookup='uri')
+groupchat_router = routers.NestedSimpleRouter(router, r'groups')  # lookup= 'groupchat'
+groupchat_router.register(r'chat', views.GroupChatViewSet, base_name='chat')
+
+messages_router = routers.NestedSimpleRouter(groupchat_router, r'chat')
+messages_router.register(r'messages', views.GroupChatMessageViewSet, base_name='messages')
+
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^', include(group_router.urls)),
-    #url(r'^', include(children_router.urls)),
+    url(r'^', include(groupchat_router.urls)),
 ]
-
-#urlpatterns += router.urls
