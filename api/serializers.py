1bef1a684137a47925ff272f69d203a2505b7492
Dimitris Stefanakis <jimstef@outlook.com>
2018-11-08 00:41:37 +0200

    Added GroupChatSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from accounts.models import UserProfile
 from groups.models import Group
+from groupchat.models import GroupMessage,GroupChat
 import json
 
 
@@ -44,22 +45,37 @@ class UserAdminProfileSerializer(serializers.ModelSerializer):
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ['id', 'owner', 'parents', 'children', 'name', 'uri', 'children_uri_field']
-        read_only_fields = ['id', 'owner', 'parents', 'children', 'name', 'uri', 'children_uri_field']
+        fields = ['id', 'owner', 'parents', 'children', 'name', 'uri', 'children_uri_field', 'description',
+                  'group_image', 'group_banner']
+        read_only_fields = ['id', 'owner', 'parents', 'children', 'name', 'uri', 'children_uri_field', 'description',
+                            'group_image', 'group_banner']
 
     children_uri_field = serializers.SerializerMethodField('children_uri')
 
     def children_uri(self, group):
         _group = Group.objects.get(uri=group)
-        print(_group)
         children = []
         for child in _group.children.all():
             children.append(child.uri)
         return children
 
 
-class ChildrenSerializer(serializers.ModelSerializer):
+class GroupChatSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Group
-        fields = ['id', 'owner', 'parents', 'children', 'name', 'uri', 'children_uri_field']
-        read_only_fields = ['id', 'owner', 'parents', 'children', 'name', 'uri', 'children_uri_field']
+        model = GroupChat
+        fields = ['id', 'type', 'name', 'group', 'group_messages']
+
+    group_messages = serializers.SerializerMethodField('group_message')
+
+    def group_message(self, groupchat):
+        _groupchat = GroupChat.objects.get(id=groupchat.id)
+        messages = []
+        for message in _groupchat.messages.all():
+            messages.append(message.message)
+        return messages
+
+
+class GroupMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupMessage
+        fields = '__all__'
