e9883d4b533e1c587f66d1d781f56451f755ce52
Dimitris Stefanakis <jimstef@outlook.com>
2018-11-08 00:40:25 +0200

    Added GroupChatViewSet

--- api/views.py
+++ api/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, mixins
 from rest_framework import authentication, permissions
-from rest_framework.pagination import LimitOffsetPagination,PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination, CursorPagination
 from accounts.models import UserProfile
 from groups.models import Group
+from groupchat.models import GroupChat, GroupMessage
 from . import serializers
 
 
@@ -13,8 +14,9 @@ class CaseInsensitiveLookupMixin(object):
     Stole majority of this mixin
     from http://www.django-rest-framework.org/api-guide/generic-views/
     """
+
     def get_object(self):
-        queryset = self.get_queryset()             # Get the base queryset
+        queryset = self.get_queryset()  # Get the base queryset
         queryset = self.filter_queryset(queryset)  # Apply any filter backends
         filter = {self.lookup_field: self.kwargs[self.lookup_field].lower()}
 
@@ -88,6 +90,7 @@ class GroupViewSet(mixins.RetrieveModelMixin,
         queryset = Group.objects.none()
         if self.kwargs['uri']:
             queryset = Group.objects.filter(uri__iexact=self.kwargs['uri'])
+            print(queryset)
         return queryset
 
 
@@ -110,9 +113,37 @@ class ChildrenViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         queryset = Group.objects.none()
-        if self.kwargs['nested_1_uri']:
-            children = Group.objects.get(uri__iexact=self.kwargs['nested_1_uri']).children.all()
+        if self.kwargs['nested_1__uri']:
+            children = Group.objects.get(uri__iexact=self.kwargs['nested_1__uri']).children.all()
             queryset = children
         return queryset
 
 
+class GroupChatViewSet(viewsets.GenericViewSet,
+                       mixins.RetrieveModelMixin,
+                       mixins.ListModelMixin):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    serializer_class = serializers.GroupChatSerializer
+    lookup_field = 'name'
+
+    def get_queryset(self):
+        queryset = GroupChat.objects.none()
+        if self.kwargs['name']:
+            group = Group.objects.get(uri__iexact=self.kwargs['nested_1__uri'])
+            queryset = GroupChat.objects.filter(name__iexact=self.kwargs['name'], group=group)
+        return queryset
+
+
+class GroupChatMessageViewSet(viewsets.GenericViewSet,
+                           mixins.RetrieveModelMixin,
+                           mixins.ListModelMixin):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    serializer_class = serializers.GroupMessageSerializer
+
+    def get_queryset(self):
+        queryset = GroupMessage.objects.none()
+        if self.kwargs['nested_2__name']:
+            group = Group.objects.get(uri__iexact=self.kwargs['nested_1__uri'])
+            group_chat = GroupChat.objects.get(name__iexact=self.kwargs['nested_2__name'], group=group)
+            queryset = GroupMessage.objects.filter(group_chat=group_chat)
+        return queryset
