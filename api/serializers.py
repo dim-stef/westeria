fe12fcfe202e2540a5e2ef4b7ed737beecef8e57
Dimitris Stefanakis <jimstef@outlook.com>
2018-11-27 15:44:44 +0200

    Get token serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,19 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from accounts.models import UserProfile
+from accounts.models import UserProfile, User
 from groups.models import Group
-from groupchat.models import GroupMessage,GroupChat
+from groupchat.models import GroupMessage, GroupChat
 import json
+
+
+class TokenSerializer(serializers.Serializer):
+    token = serializers.CharField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -29,6 +39,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
         instance.profile_image = validated_data.get('profile_image', instance.profile_image)
         instance.save()
         return instance
+
+
+class UserPublicProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['url', 'profile_image']  # '__all__'
+        read_only_fields = ['url', 'profile_image']
 
 
 class UserAdminProfileSerializer(serializers.ModelSerializer):
@@ -78,4 +95,14 @@ class GroupChatSerializer(serializers.ModelSerializer):
 class GroupMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = GroupMessage
-        fields = '__all__'
+        fields = ['author', 'author_name', 'author_url', 'message', 'created', 'updated', 'group_chat']
+
+    author_name = serializers.SerializerMethodField('author_name_field')
+    author_url = serializers.SerializerMethodField('author_url_field')
+
+    def author_name_field(self, groupmessage):
+        return groupmessage.author.full_name
+
+    def author_url_field(self, groupmessage):
+        return groupmessage.author.profile.url
+
