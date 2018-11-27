ac37eed8fdc279d43b0b7c27bb5e6bfe62e24193
Dimitris Stefanakis <jimstef@outlook.com>
2018-11-27 15:54:27 +0200

    Updated imports

--- react/src/js/components/container/AppContainer.js
+++ react/src/js/components/container/AppContainer.js
@@ -1,33 +1,31 @@
 import React, { Component } from "react";
 import { Switch, Route, Link  } from 'react-router-dom'
-import { withRouter } from 'react-router'
-import { SideBar } from '../home'
-import { Tree, Test } from "./GroupContainer"
+import { withRouter , Redirect } from 'react-router'
+import { Page } from '../Page'
+import { Tree, Test } from "../presentational/Tree"
 import GroupHomeContainer from "../container/GroupHomeContainer"
-import GroupChatContainer from "../container/GroupChatContainer"
+import GroupChatContainer, {GroupChatMessagesContainer} from "../container/GroupChatContainer"
 import { SettingsContainer } from "./SettingsContainer"
+import axios from 'axios'
 
 
-
-
-const Routes = () => (
-    <SideBar>
+const Routes = (props) => (
+    <Page>
         <Switch>
             <Route exact path='/' component={(props) => <Test {...props} />}  />
-            <Route exact path='/settings' component={(props) => <SettingsContainer {...props} />}  />
+            <Route exact path='/settings' component={(props) => <SettingsContainer {...props}/> }  />
             <Route path='/map/:uri?' component={(props) => <Tree {...props} root={"global"} key="Tree" />} />
-            <Route path='/:uri?/chat/:roomName?' component={(props) => <GroupChatContainer {...props} />} />
+            <Route path='/:uri?/chat/:roomName?' component={(props) => <GroupChatMessagesContainer {...props} />} />
             <Route path='/:uri?' component={(props) => <GroupHomeContainer {...props} />} />
             
         </Switch>
-    </SideBar>
+    </Page>
 )
 
 class App extends Component {
     constructor(props){
         super(props)
 
-        console.log(this.props)
         this.unlisten = this.props.history.listen((location, action) => {
             console.log("on route change");
         });
