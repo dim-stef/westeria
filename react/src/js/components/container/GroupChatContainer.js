90cf3b0d7ffa3d99cfac94f5751870ce47199c54
Dimitris Stefanakis <jimstef@outlook.com>
2018-11-27 15:55:06 +0200

    Added GroupChatMessagesContainer

--- react/src/js/components/container/GroupChatContainer.js
+++ react/src/js/components/container/GroupChatContainer.js
@@ -1,20 +1,128 @@
 import React, { Component } from "react";
 import axios from "axios";
+import GroupChatMessage, { GroupChatMessageBox } from "../GroupChatMessage";
+const uuidv1 = require('uuid/v1');
+
+
+export class GroupChatMessagesContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [],
+            ws: null,
+            group: this.props.group,//this.props.group,
+            roomName: this.props.roomName,//this.props.roomName,
+            hasMore: false,
+            next: null,
+        }
+
+        this.prepareState = this.prepareState.bind(this);
+    }
+
+    async getData(next) {
+        var uri;
+        if (next !== null) {
+            uri = next;
+        }
+        else {
+            uri = `/api/groups/${this.state.group.uri}/chat/${this.state.roomName}/messages/`;
+        }
+
+        var response = await axios.get(uri, { withCredentials: true });
+        return response.data;
+    }
+
+    async prepareState() {
+        var messages;
+        messages = await this.getData(this.state.next);
+
+        var next = messages.next
+        var hasMore = true;
+        if (next === null) {
+            hasMore = false;
+        }
+        messages = messages.results;
+        messages = messages.reverse()
+        this.setState({
+            messages: messages,
+            next: next,
+            hasMore: hasMore,
+            ws: new WebSocket(`ws://${window.location.host}/ws/chat/${this.state.group.uri}/`)
+        })
+
+    }
+
+    componentDidMount() {
+        var self = this;
+        this.prepareState();
+
+        window.onscroll = function () {
+            if (window.pageYOffset === 0) { // use .scrollTop for other elements
+                if (self.state.hasMore) {
+                    self.prepareState();
+                }
+            }
+        }
+    }
+
+    componentWillUnmount() {
+        window.onscroll = null;
+    }
+
+    render() {
+        if (this.state.messages && this.state.ws) {
+            console.log(this.state.messages)
+            return (
+                <GroupChatContainer ws={this.state.ws} roomName={this.state.roomName} messages={this.state.messages} group={this.state.group.id} />
+            )
+        }
+        return null;
+
+    }
+
+}
+
 
 class GroupChatContainer extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            roomName: this.props.params.uri,
+            group: this.props.group,
+            roomName: this.props.roomName,
             currentMessage: '',
-            messages: [],
-            ws: new WebSocket(`ws://${window.location.host}/ws/chat/${this.props.params.uri}/`),
+            files:'',
+            messages: this.props.messages,
+            ws: this.props.ws,
         }
 
         this.inputHandler = this.inputHandler.bind(this);
         this.submitHandler = this.submitHandler.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.drop = this.drop.bind(this);
+        this.preventDefault = this.preventDefault.bind(this);
+    }
+
+
+    preventDefault(e) {
+        e.preventDefault();
+    }
+
+    drop(event) {
+
+        event.preventDefault();
+        var data = event.dataTransfer.items;
+        console.log(data);
+        var files = [];
+        for (var i = 0; i < data.length; i++) {
+            files.push(data[i].getAsFile());
+        }
+        console.log(files)
+        /*try {
+            
+        } catch (e) {
+            return;
+        }*/
     }
 
     inputHandler(e) {
@@ -25,89 +133,123 @@ class GroupChatContainer extends Component {
         var message = this.state.currentMessage;
         if (this.state.currentMessage.replace(/\s/g, "").length > 0) {
             this.state.ws.send(JSON.stringify({
-                'message': message
+                'message': message,
+                'room_name': this.state.roomName,
+                'group': this.state.group
             }));
         }
     }
 
     handleKeyPress(event) {
-        if(event.key == 'Enter' && !event.shiftKey){
+        if (event.key == 'Enter' && !event.shiftKey) {
             event.preventDefault();
             var message = this.state.currentMessage.slice();
             var dummy = this.state.currentMessage.slice();
             if (dummy.replace(/\s/g, "").length > 0) {
                 this.state.ws.send(JSON.stringify({
-                    'message': message
+                    'message': message,
+                    'room_name': this.state.roomName,
+                    'group': this.state.group
                 }));
             }
             this.setState({ currentMessage: '' })
+            this.el.scrollIntoView({ behavior: "instant" });
         }
     }
 
-    componentDidUpdate() {
-        this.el.scrollIntoView({ behavior: "instant" });
+    onlyUnique(value, index, self) {
+        return self.indexOf(value) === index;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        var concatenatedMessages = nextProps.messages.concat(this.state.messages);
+        var concatenatedUnique = concatenatedMessages.filter(this.onlyUnique)
+        this.setState({ messages: concatenatedUnique });
     }
 
     componentDidMount() {
         var self = this;
-        /*this.state.ws.onopen = function(){
-            alert("open");
-        }*/
-
+        this.el.scrollIntoView({ behavior: "instant" });
         this.state.ws.onmessage = function (e) {
             var data = JSON.parse(e.data);
             var message = data['message'];
-            console.log(message);
-            var name = data['name'];
-            console.log(e.data);
+            var author_name = data['author_name'];
+            var author_url = data['author_url'];
+            var author = data['author'];
             var bundle = {
                 message: message,
-                name: name
+                author_name: author_name,
+                author_url: author_url,
+                author: author
             }
             var newMessages = self.state.messages.slice();
             newMessages.push(bundle);
             self.setState({ messages: newMessages })
+            self.el.scrollIntoView({ behavior: "instant" });
         };
     }
 
-    render() {
-        var messages = this.state.messages.map((m) => {
-            return <p>{m.name}: {m.message}</p>
+    getMessageBoxes() {
+        var chatBox = {
+            author: null,
+            author_url: null,
+            messages: []
+        };
+        var messageBoxes = this.state.messages.map((m, i) => {
+            var nextAuthor = null;
+            chatBox.author = m.author;
+            chatBox.author_url = m.author_url;
+            chatBox.messages.push(m.message)
+
+            if (i < this.state.messages.length - 1) {
+                nextAuthor = this.state.messages[i + 1].author_url
+            }
+
+            if (m.author_url !== nextAuthor) {
+                var copy = Object.assign({}, chatBox);
+                chatBox.author_url = null;
+                chatBox.messages = [];
+                return copy;
+            }
+            return null;
         })
+        var filtered = messageBoxes.filter(function (el) {
+            return el != null;
+        });
+        messageBoxes = filtered.map(m => {
+            return <GroupChatMessageBox messageBox={m} />
+        })
+        return messageBoxes;
+    }
+
+    render() {
+        var messageBoxes = this.getMessageBoxes();
         return (
-            <div>
-                <div id="chat-log" style={{ fontSize: '1.7em',whiteSpace: 'pre-line' }}>{messages}</div><br />
-                <textarea onChange={this.inputHandler} onKeyPress={this.handleKeyPress} style={{resize:'none', width:300, height:80}} rows="1" value={this.state.currentMessage} id="chat-message-input"></textarea>
-                <br/>
-                <input onClick={this.submitHandler} id="chat-message-submit" type="button" value="Send" />
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <div id="chat-log" className="chat-log">
+                    {messageBoxes}
+                </div>
+
+                <div style={{ position: 'fixed', bottom: 0, height: 100, width: '100%' }}>
+                    <textarea onDrop={this.drop} onDragOver={this.preventDefault} onChange={this.inputHandler} onKeyPress={this.handleKeyPress} className="text-wrapper" rows="1" value={this.state.currentMessage} id="chat-message-input"></textarea>
+                </div>
                 <div ref={el => { this.el = el; }}></div>
             </div>
         )
     }
 }
 
-
-class GroupChatMessagesContainer extends Component {
+class CustomTextArea extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            messages:[],
-            roomName:'',
-            socket:null
-        }
     }
 
-    async getMessages(){
-        var uri = `/api/groups/${this.state.roomName}/messages/`
-        var response = await axios.get(uri, {withCredentials:true});
-        
+    render(){
+        return(
+            <textarea onDrop={this.drop} onDragOver={this.preventDefault} onChange={this.inputHandler} onKeyPress={this.handleKeyPress} className="text-wrapper" rows="1" value={this.props.value} id="chat-message-input"></textarea>
+        )
     }
-
-    componentDidMount(){
-        
-
-    }
-
 }
 
+//<input onClick={this.submitHandler} id="chat-message-submit" type="button" value="Send" />
 export default GroupChatContainer;
