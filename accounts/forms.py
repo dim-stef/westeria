807a91758fd04bcc084c3187aacf0c0c41cf405c
Dimitris <jimstef@outlook.com>
2018-12-11 00:42:32 +0200

    Now user creates branch on signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,25 @@
 from django import forms
+from allauth.account.forms import SignupForm
+from .models import UserProfile
+from branches.models import Branch
+from random import randint
 
+def unique_tag():
+    while True:
+        tag = randint(100, 999)
+        if not Branch.objects.filter(tag=tag).exists():
+            return tag
 
-class SignupForm(forms.Form):
+class CustomSignupForm(SignupForm):
 
-    first_name = forms.CharField(max_length=30, label='First name', required=True, widget=forms.TextInput(
+    name = forms.CharField(max_length=24, label='name', required=True, widget=forms.TextInput(
                     attrs={
-                        'placeholder': 'First name'
+                        'placeholder': 'name'
                     }
     ))
-    last_name = forms.CharField(max_length=30, label='Last name', required=True,widget=forms.TextInput(
-                    attrs={
-                        'placeholder': 'Last name'
-                    }
-    ))
 
-    def signup(self, request, user):
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.save()
+    def save(self, request):
+        user = super().save(request)
+        profile_group = Branch.objects.create(owner=user, name=self.cleaned_data['name'], tag=unique_tag())
+        UserProfile.objects.create(user=user,name=self.cleaned_data['name'], url=profile_group.uri)
+        return user
