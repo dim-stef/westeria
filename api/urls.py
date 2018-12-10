16ae9c0217d1629b652972568e9bba0a13ac4014
Dimitris <jimstef@outlook.com>
2018-12-11 00:45:24 +0200

    Reformatted names

--- api/urls.py
+++ api/urls.py
@@ -6,24 +6,26 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'user', views.UserViewSet, base_name='user')
+router.register(r'owned_branches', views.OwnedBranchesViewSet, base_name='owned_branches')
 router.register(r'profile', views.UserProfileViewSet, base_name='profile')
-router.register(r'public_profile', views.UserPublicProfileViewSet, base_name='public_profile')
-router.register(r'ROOT', views.GroupRootViewSet, base_name='grouproot')
-router.register(r'groups', views.GroupViewSet, base_name='group')  # lookup= 'groups'
+router.register(r'public_profile', views.BranchPublicProfileSerializer, base_name='public_profile')
+router.register(r'ROOT', views.BranchRootViewSet, base_name='branchroot')
+router.register(r'branches', views.BranchViewSet, base_name='branch')  # lookup= 'branches'
 
-group_router = routers.NestedSimpleRouter(router, r'groups')  # lookup= 'children'
-group_router.register(r'children', views.ChildrenViewSet, base_name='children')
+branch_router = routers.NestedSimpleRouter(router, r'branches')  # lookup= 'children'
+branch_router.register(r'children', views.ChildrenViewSet, base_name='children')
 
-groupchat_router = routers.NestedSimpleRouter(router, r'groups')  # lookup= 'groupchat'
-groupchat_router.register(r'chat', views.GroupChatViewSet, base_name='chat')
+branchchat_router = routers.NestedSimpleRouter(router, r'branches')  # lookup= 'branchchat'
+branchchat_router.register(r'chat', views.BranchChatViewSet, base_name='chat')
 
-messages_router = routers.NestedSimpleRouter(groupchat_router, r'chat')
-messages_router.register(r'messages', views.GroupChatMessageViewSet, base_name='messages')
+messages_router = routers.NestedSimpleRouter(branchchat_router, r'chat')
+messages_router.register(r'messages', views.BranchChatMessageViewSet, base_name='messages')
 
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^', include(group_router.urls)),
-    url(r'^', include(groupchat_router.urls)),
+    url(r'^', include(branch_router.urls)),
+    url(r'^', include(branchchat_router.urls)),
+    url(r'^', include(messages_router.urls)),
     path('token/', views.CreateToken.as_view())
 ]
